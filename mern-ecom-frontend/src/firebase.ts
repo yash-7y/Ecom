92b1879111c2c1eef1587b8fe640170314cb104e
yash-7y <yash37y@gmail.com>
2025-07-20 19:54:31 +0530

    Update firebase.ts

--- mern-ecom-frontend/src/firebase.ts
+++ mern-ecom-frontend/src/firebase.ts
@@ -9,6 +9,6 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
 };
-console.log("VITE_FIREBASE_KEY at runtime:", import.meta.env.VITE_FIREBASE_KEY);
+// console.log("VITE_FIREBASE_KEY at runtime:", import.meta.env.VITE_FIREBASE_KEY); fr debugging
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
